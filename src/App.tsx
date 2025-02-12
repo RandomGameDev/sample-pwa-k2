2fd1fa129bee096a02ca4d82dee6fbfe374abc7c
Knowell Cedrich Riego <knowell.riego@cloudeats.com>
2025-02-12 12:45:40 +0800

    Update the tailwind configuration to v4

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { RobinMain } from "@cloudeats/robin-components";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
@@ -14,9 +13,6 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
