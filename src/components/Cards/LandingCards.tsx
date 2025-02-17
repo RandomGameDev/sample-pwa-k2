4301a5fe8f201b021d021ba8a6f4ee48bbe0ddd7
ahmad.rusli <ahmad.rusli@cloudeats.com>
2025-02-17 11:11:02 +0700

    NEO-87 Updated the button

--- src/components/Cards/LandingCards.tsx
+++ src/components/Cards/LandingCards.tsx
@@ -1,28 +1,31 @@
+import { Button } from "../Button/Button.tsx";
 import { cn } from "@cloudeats/robin-components";
 import { Link } from "react-router";
 import card1 from "../../assets/card1.jpg";
 
 export const LandingCards = () => {
   return (
-    <div className={cn("px-4 py-6")}>
-      <Link to="/menu">
-        <img
-          className="h-48 w-full rounded-t-lg object-cover"
-          alt="Sulit Chicken"
-          src={card1}
-        />
-        <div className="flex flex-col justify-between rounded-lg p-4 text-left shadow-lg">
-          <div className="flex justify-between">
+    <div className={cn("relative px-4 py-6")}>
+      <img
+        className="h-48 w-full rounded-t-lg object-cover"
+        alt="Sulit Chicken"
+        src={card1}
+      />
+      <div className="flex flex-col justify-between rounded-lg p-4 text-left shadow-lg">
+        <div className="group flex justify-between">
+          <div className="ltr:ml-3">
             <h2 className="text-2xl font-semibold">Sulit Chicken</h2>
-            <button className="rounded-lg bg-yellow-500 px-4 py-2 font-semibold hover:bg-yellow-700">
-              <span className="text-lg">&gt;</span>
-            </button>
+            <p className="text-gray-600">
+              Enjoy the best flavored fried chicken na todo sulit, todo sarap!
+            </p>
           </div>
-          <p className="text-gray-600">
-            Enjoy the best flavored fried chicken na todo sulit, todo sarap!
-          </p>
+          <Link to="/menu">
+            <Button variant={"warning"} style={{ cursor: "pointer" }}>
+              <span className="text-lg">&gt;</span>
+            </Button>
+          </Link>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
