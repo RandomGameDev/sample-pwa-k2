58735989d8c2d2e6bcef4aead89bb5f370911d5b
ahmad.rusli <ahmad.rusli@cloudeats.com>
2025-02-20 17:06:57 +0700

    NEO-141 Initiated dish details page

---
+++ src/pages/dishDetails/DishDetailsPage.tsx
@@ -0,0 +1,103 @@
+import { Button } from "@components/Button/Button";
+import { cn } from "@cloudeats/robin-components";
+import { useState } from "react";
+import landing1 from "../../assets/landing1.png";
+
+interface Addon {
+  id: number;
+  name: string;
+  price: number;
+}
+
+const addons: Addon[] = [
+  { id: 1, name: "Sulit Chicken", price: 99.99 },
+  { id: 2, name: "Todo Sarap Burger", price: 129.99 },
+  { id: 3, name: "Fried Rice", price: 49.99 },
+  { id: 4, name: "Soft Drink", price: 29.99 },
+];
+
+export const DishDetailsPage = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleDecrement = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+
+  return (
+    <div className={cn("px-0 py-6")}>
+      <img src={landing1} alt="" className="w-full" />
+      <div className={cn("px-4 py-2")}>
+        <div className="my-4 flex items-end justify-between">
+          <h1 className="text-4xl">Premium Soy Garlic Karaage</h1>
+          <span className="text-4xl">₱ 249.00</span>
+        </div>
+        <p className="my-4 text-2xl text-neutral-500">
+          This will be the part where the description of the food will display
+          but we will still need to confirm up to how many characters this is.
+        </p>
+        <p className="my-4">
+          Add-ons{" "}
+          <span className={cn("text-neutral-500")}>(optional, max 5)</span>:
+        </p>
+        {addons.map((addon) => (
+          <div
+            key={addon.id}
+            className={cn("addons-center my-2 flex justify-between")}
+          >
+            <div className={cn("addons-center flex justify-between gap-2")}>
+              <div className={cn("flex items-center gap-4")}>
+                <input type="checkbox" className={cn("h-4 w-4")} />
+                <span className={cn("text-lg")}>{addon.name}</span>
+              </div>
+            </div>
+            <span className={cn("text-lg font-semibold")}>
+              +₱{addon.price.toFixed(2)}
+            </span>
+          </div>
+        ))}
+      </div>
+      <hr className="border-6 border-e-neutral-500 border-b-neutral-200" />
+      <div className={cn("px-4 py-2")}>
+        <p className="my-2 text-2xl">
+          Remarks <span className={cn("text-neutral-500")}>(Optional)</span>:
+        </p>
+        <textarea
+          className={cn(
+            "h-20 w-full rounded-md border border-neutral-300 pt-2 pr-4 pl-4 text-xl"
+          )}
+          placeholder="Please remove onions if there any."
+        />
+      </div>
+      <div className={cn("flex items-center justify-center gap-6")}>
+        <Button
+          className={cn(
+            "bg-primary-500 hover:bg-primary-700 h-20 w-20 rounded-full text-4xl"
+          )}
+          onClick={handleDecrement}
+        >
+          -
+        </Button>
+        <span className={cn("text-4xl font-semibold")}>{quantity}</span>
+        <Button
+          className={cn(
+            "bg-primary-500 hover:bg-primary-700 h-20 w-20 rounded-full text-4xl"
+          )}
+          onClick={handleIncrement}
+        >
+          +
+        </Button>
+      </div>
+      <div className={cn("px-4 py-2")}>
+        <Button className="border-primary-500 bg-primary-500 mt-4 w-full rounded-lg border-2 py-6 text-2xl text-black">
+          Add to Basket - ₱ 249.00
+        </Button>
+      </div>
+    </div>
+  );
+};
